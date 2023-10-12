33c8ea10365ff4b4e4b1af2daf38b07b4a6ffabf
Hasibul Hasan <hasibul.af@gmail.com>
2023-10-13 01:03:18 +0600

    Server Started

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const users = [
+        {id: 1, name: 'Sabana', email: 'sabana@mail.com'},
+        {id: 2, name: 'Sabnur', email: 'sabnur@mail.com'},
+        {id: 3, name: 'Sabila', email: 'sabila@mail.com'},
+]
+
+app.get('/', (req, res) => {
+  res.send('Users Management Server is Running')
+})
+
+app.get('/users', (req, res) =>{
+    res.send(users);
+})
+
+app.post('/users', (req, res) =>{
+    console.log('Post API Hitting');
+    console.log(req.body);
+})
+
+app.listen(port, () => {
+  console.log(`Server is iRunning on PORT: ${port}`)
+})
