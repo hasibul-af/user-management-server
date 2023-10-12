cf592294a215cb2e1e2f7bf271de46599488b7db
Hasibul Hasan <hasibul.af@gmail.com>
2023-10-13 01:27:47 +0600

    Complete Some Server Learning

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 const users = [
         {id: 1, name: 'Sabana', email: 'sabana@mail.com'},
@@ -22,6 +23,10 @@ app.get('/users', (req, res) =>{
 app.post('/users', (req, res) =>{
     console.log('Post API Hitting');
     console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
 })
 
 app.listen(port, () => {
